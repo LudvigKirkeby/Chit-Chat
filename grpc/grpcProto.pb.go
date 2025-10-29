96d8a834c4074ad3c03bea203c01ee8ffca87fd7
mcls <mcls@itu.dk>
2025-10-29 16:20:28 +0100

    delete unused Empty message in proto Co-authored-by: Ludvig Kirkeby <ludvig.elias.kirkeby@gmail.com> Co-authored-by: Camille <hocl@itu.dk>

--- grpc/grpcProto.pb.go
+++ grpc/grpcProto.pb.go
@@ -73,42 +73,6 @@ func (x *Message) GetText() string {
 	return ""
 }
 
-type Empty struct {
-	state         protoimpl.MessageState `protogen:"open.v1"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
-}
-
-func (x *Empty) Reset() {
-	*x = Empty{}
-	mi := &file_grpcProto_proto_msgTypes[1]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *Empty) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Empty) ProtoMessage() {}
-
-func (x *Empty) ProtoReflect() protoreflect.Message {
-	mi := &file_grpcProto_proto_msgTypes[1]
-	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Empty.ProtoReflect.Descriptor instead.
-func (*Empty) Descriptor() ([]byte, []int) {
-	return file_grpcProto_proto_rawDescGZIP(), []int{1}
-}
-
 var File_grpcProto_proto protoreflect.FileDescriptor
 
 const file_grpcProto_proto_rawDesc = "" +
@@ -116,8 +80,7 @@ const file_grpcProto_proto_rawDesc = "" +
 	"\x0fgrpcProto.proto\"B\n" +
 	"\aMessage\x12#\n" +
 	"\rlamport_clock\x18\x01 \x01(\x03R\flamportClock\x12\x12\n" +
-	"\x04text\x18\x02 \x01(\tR\x04text\"\a\n" +
-	"\x05Empty22\n" +
+	"\x04text\x18\x02 \x01(\tR\x04text22\n" +
 	"\x0eMessageService\x12 \n" +
 	"\x04Join\x12\b.Message\x1a\b.Message\"\x00(\x010\x01B\x19Z\x17handin-3/grpc/grpcProtob\x06proto3"
 
@@ -133,10 +96,9 @@ func file_grpcProto_proto_rawDescGZIP() []byte {
 	return file_grpcProto_proto_rawDescData
 }
 
-var file_grpcProto_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_grpcProto_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_grpcProto_proto_goTypes = []any{
 	(*Message)(nil), // 0: Message
-	(*Empty)(nil),   // 1: Empty
 }
 var file_grpcProto_proto_depIdxs = []int32{
 	0, // 0: MessageService.Join:input_type -> Message
@@ -159,7 +121,7 @@ func file_grpcProto_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_grpcProto_proto_rawDesc), len(file_grpcProto_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
